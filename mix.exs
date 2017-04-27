80a40a938dd2a993325aace42000b3df2e9d616b
Anderson Luiz Vendruscolo <andersonlven@gmail.com>
2017-04-27 15:36:25 -0300

    descrição pacote

--- mix.exs
+++ mix.exs
@@ -7,12 +7,32 @@ defmodule MoipEx.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     source_url: "https://github.com/AnderLuiz/moip_ex",
      deps: deps()]
   end
 
   def application do
     [extra_applications: [:logger]]
   end
+
+  defp description do
+    """
+    Lib para lidar com o Moip pagamentos (https://www.moip.com.br/).
+    """
+  end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      name: :moip_ex,
+      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      maintainers: ["Anderson Luiz Vendruscolo"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/AnderLuiz/moip_ex"}
+    ]
+  end
+
+
 
   def deps do
     [
