39a9dcd7b1ab47ba8bf14a3408bd4f6c38ca24f0
Anderson Luiz Vendruscolo <andersonlven@gmail.com>
2017-06-22 15:51:13 -0300

    enviroment variable em no modo dev

--- config/dev.exs
+++ config/dev.exs
@@ -1,3 +1,9 @@
 use Mix.Config
 
-import_config "dev.secret.exs"
+
+config :moip_ex,
+  token: System.get_env("TOKEN"),
+  api_key: System.get_env("API_KEY"),
+  env: :sandbox
+
+#import_config "dev.secret.exs"
